3b13d2d3fdc9144bb14440992edc87e360cefc6a
Mathiazom <mathiasomyklebust@gmail.com>
2020-06-09 17:05:18 +0200

    Some serious refactoring

--- script.js
+++ script.js
@@ -1,71 +1,94 @@
-function init(){
+
+var pods = new Array();
+
+window.onload = function(){
+
+  document.getElementById('splash_text').innerHTML =
+    getRandomArrayItem(bjarteNavn) + ", " +
+    getRandomArrayItem(toreNavn) + " og " +
+    getRandomArrayItem(steinarNavn)
+
   loadPods();
-  document.getElementById('play_button').addEventListener("click",function(){
-    var podIndex = Math.floor(Math.random() * podArray.length);
-    playPod(podArray[podIndex]);
+
+  document.getElementById('random_play_button').addEventListener("click",function(){
+    playPod(getRandomArrayItem(pods));
   })
+
 }
 
 function playPod(pod){
-  document.getElementById('podcast_audio').src = pod.enclosure.link;
-  document.getElementById('current_playing_pod').innerHTML = "Spiller nå " + pod.title;
-  document.getElementById('current_playing_pod').style.display = "block";
+
+  document.getElementById('podcast_audio').src = pod.enclosures[0].url;
+
+  document.getElementById('current_playing_pod').innerHTML = pod.title;
+  document.getElementById('current_playing_div').style.display = "block";
+
+  $('html, body').animate({scrollTop: 0}, 200);
+
 }
 
-let podArray = new Array();
-
 function loadPods(){
-  var url= 'http://podkast.nrk.no/program/radioresepsjonen.rss';
+  let url= 'http://podkast.nrk.no/program/radioresepsjonen.rss';
   $.ajax({
     type: 'GET',
     url: "https://morning-earth-19323.herokuapp.com/?feedURL=" + url,
     dataType: 'json',
-    success: function(data) {
-      console.log(data);
-      for(item of data.items){
-        var podLink = item.enclosures[0].url;
-        var podIndex = data.items.indexOf(item);
-        podArray[podIndex] = item;
-
-        var podRow = document.createElement("tr");
-        podRow.className = "podlist_row";
-        podRow.addEventListener("click",function(){
-          playPod(podArray[podIndex]);
-        })
-
-        var podTitle = document.createElement("td");
-        podTitle.className = "podlist_item"
-        podTitle.innerHTML = item.title;
-
-        var podDate = document.createElement("td");
-        podDate.className = "podlist_item"
-        podDate.innerHTML = new Date(item.created);
-
-        var podDuration = document.createElement("td");
-        podDuration.className = "podlist_item"
-        podDuration.innerHTML = item.enclosures[0].length;
-
-        podRow.appendChild(podTitle);
-        podRow.appendChild(podDate);
-        podRow.appendChild(podDuration);
-
-        document.getElementById('podlist_table').appendChild(podRow);
-      }
-    }
+    success: handlePodsData
   });
 }
 
-/*
-function positionMainDiv(){
-  var height = window.innerHeight
-  || document.documentElement.clientHeight
-  || document.body.clientHeight;
+function handlePodsData(data){
 
-  var main_div = document.getElementById('main_div');
+  pods = data.items;
 
-  var topOffset = ((height/2)-(main_div.clientHeight))+ "px";
+  let podsTable = document.getElementById('podlist_table');
 
-  main_div.style.marginTop = topOffset;
+  for(pod of pods){
+
+    podsTable.appendChild(createPodRow(pod));
+
+  }
+
+  setClassDisplay("post_pods_load", "block");
+  setClassDisplay("pre_pods_load", "none");
+
 }
-*/
-init();
+
+function createPodRow(pod){
+
+  let podRow = document.createElement("tr");
+  podRow.className = "podlist_row";
+  podRow.addEventListener("click",function(){
+    playPod(pod);
+  })
+
+  let podData = document.createElement("td");
+  podData.className = "podlist_item";
+
+  let podTitle = document.createElement("p");
+  podTitle.className = "podlist_title";
+  podTitle.innerHTML = pod.title;
+  podData.appendChild(podTitle);
+
+  let podDescription = document.createElement("p");
+  podDescription.className = "podlist_description";
+  podDescription.innerHTML = pod.description;
+  podData.appendChild(podDescription);
+
+  podRow.appendChild(podData);
+
+  return podRow;
+
+}
+
+
+function getRandomArrayItem(array){
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function setClassDisplay(className, display){
+  let elements = document.getElementsByClassName(className);
+  for(el of elements){
+    el.style.display = display;
+  }
+}
